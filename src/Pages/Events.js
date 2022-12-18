10f11005f0129ddac7629405b34a41f43a2d605f
Rahul singh <rahulsingh19390@gmail.com>
2022-12-18 14:31:32 +0530

    Updated some specific changes for TC

---
+++ src/Pages/Events.js
@@ -0,0 +1,9 @@
+import MotionHoc from "./MotionHoc";
+
+const EventsComponent = () => {
+  return <h1>Events</h1>;
+};
+
+const Events = MotionHoc(EventsComponent);
+
+export default Events;
---
+++ src/Pages/Journey.js
@@ -0,0 +1,9 @@
+import MotionHoc from "./MotionHoc";
+
+const JourneyComponent = () => {
+  return <h1>Our Journey</h1>;
+};
+
+const Journey = MotionHoc(JourneyComponent);
+
+export default Journey;
